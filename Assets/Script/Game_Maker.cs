b105e36c3ef44c98c5343d8523162a1987e547e6
RezaParsian <rezaparsian76@gmail.com>
2021-01-10 14:50:28 +0330

    my first game with unity

---
+++ Assets/Script/Game_Maker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Maker : MonoBehaviour
+{
+    public string[] jobs;
+    public GameObject player, object1, object2, object3;
+    public List<Vector3> place;
+    Vector3[] newplace = new Vector3[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int t = 0; t < 3; t++)
+        {
+            int ind = Random.Range(0, place.Count);
+            newplace[t] = place[ind];
+            place.RemoveAt(ind);
+        }
+
+        int count = jobs.Length;
+        int index = Random.Range(0, count);
+        string job = jobs[index];
+
+        string o1 = jobs[Random.Range(0, count)];
+        string o2 = jobs[Random.Range(0, count)];
+
+        while (o1 == job || o2 == job)
+        {
+            o1 = jobs[Random.Range(0, count)];
+            o2 = jobs[Random.Range(0, count)];
+        }
+
+        player.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(job);
+        object1.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(job + Random.Range(1, 3));
+        object1.transform.position = newplace[0];
+        object2.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(o1 + Random.Range(1, 3));
+        object2.transform.position = newplace[1];
+        object3.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(o2 + Random.Range(1, 3));
+        object3.transform.position = newplace[2];
+    }
+}
---
+++ Assets/Script/Move_Player.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move_Player : MonoBehaviour
+{
+    public float speed = 100.0f;
+    Vector3 mousePosition;
+    Rigidbody2D rb;
+    Vector2 direction;
+    public GameObject error;
+    public ParticleSystem win;
+    bool finished=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finished)
+            return;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0,speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0,-speed * Time.deltaTime, 0);
+        }
+
+
+        if (Input.GetMouseButton(0) && Input.touchCount == 1)
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 987; // select distance units from the camera
+  
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
+            direction = (mousePosition - transform.position);
+            rb.velocity = new Vector2(direction.x*speed, direction.y*speed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+        
+        
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        switch (tag)
+        {
+            case "win":
+                win.Play();
+                Destroy(collision.gameObject);
+                Invoke("Win", 5);
+                finished = true;
+               break;
+            case "try":
+                Destroy(collision.gameObject);
+                Color c = new Color(1f, 0f, 0f, 0.5f);
+                Time.timeScale = 0.1f;
+                error.GetComponent<SpriteRenderer>().color =c;
+                Invoke("FadeOut",0.2f);
+                Handheld.Vibrate();
+                break;
+        }
+    }
+
+    void Win()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void FadeOut()
+    {
+        Color c = new Color(1f, 0f, 0f, 0f);
+        Time.timeScale = 1f;
+        error.GetComponent<SpriteRenderer>().color = c;
+    }
+
+}
